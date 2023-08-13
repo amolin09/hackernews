d0af2fd093f02e33a3dcfe92848ac58dbe55345e
unknown <xander.mcgowan@gmail.com>
2023-08-13 15:51:26 -0500

    added search bar

--- hackernews/src/App.js
+++ hackernews/src/App.js
@@ -1,31 +1,15 @@
-import React, { useState, useEffect } from "react";
-import SearchBar from './searchBar'
-import "./App.css";
+import * as React from 'react';
+import { PrimarySearchAppBar } from './searchBar';
+
+
 
 function App() {
-  const [news, setNews] = useState();
-
-  useEffect(() => {
-    const getNews = async () => {
-      const response = await fetch("http://hn.algolia.com/api/v1/items/2");
-      const data = await response.json();
-      setNews(data);
-    };
-    getNews();
-  }, []);
-
-  console.log(news)
-
   return (
-    <div>
-
-<div classname='App'>
-       <SearchBar/>
+    <div classname='App'>
+       <PrimarySearchAppBar></PrimarySearchAppBar>
      </div>
 
-  
 
-    </div>
   );
 }
 
